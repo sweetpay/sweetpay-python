060284a55f240650097b69f20b8ac4782f483124
David Buresund <david.buresund@gmail.com>
2017-02-12 23:27:00 +0100

    Fixed silly setup mistake

--- Makefile
+++ Makefile
@@ -7,3 +7,10 @@ setupdev:
 shell:
 	ipython -i example.py
 
+pypitest:
+	python setup.py register -r pypitest
+	python setup.py sdist upload -r pypitest
+
+pypiprod:
+	python setup.py register -r pypi
+	python setup.py sdist upload -r pypi
--- setup.py
+++ setup.py
@@ -1,22 +1,12 @@
-import os
 from setuptools import setup
 
-__version__ = "0.2.1"
-
-
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
+__version__ = "0.2.1.1"
 
 setup(
     name="sweetpay",
     version=__version__,
     author="David Buresund",
-    author_email="david.buresund@paylevo.com",
+    author_email="david.buresund@gmail.com",
     description="A SDK to talk with the Sweetpay APIs (alpha version)",
     license="Apache 2",
     keywords="sweetpay checkout payment",
@@ -24,5 +14,5 @@ setup(
     download_url="https://github.com/sweetpay/sweetpay-"
                  "python/tarball/%s" % __version__,
     packages=["sweetpay"],
-    install_requires=["requests==2.10.0", "python-dateutil-2.6.0"]
+    install_requires=["requests==2.10.0", "python-dateutil==2.6.0"]
 )
