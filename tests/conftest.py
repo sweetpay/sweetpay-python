c391a937447d309c34c828ed12c415f065d50a84
David Buresund <david.buresund@gmail.com>
2017-08-05 20:21:29 +0200

    Added version overwrite support

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,8 +7,26 @@ from sweetpay import SweetpayClient
 
 
 @pytest.fixture()
-def client():
+def creditcheck_version():
+    return 2
+
+
+@pytest.fixture()
+def subscription_version():
+    return 1
+
+
+@pytest.fixture()
+def checkout_session_version():
+    return 1
+
+
+@pytest.fixture()
+def client(subscription_version, creditcheck_version, checkout_session_version):
     # TODO: Create merchant and token for testing
     return SweetpayClient(
-        "paylevo", True,
-        {"subscription": 1, "creditcheck": 2, "checkout_session": 1}, 4)
+        "NNq7Rcnb8y8jGTsU", stage=True, version={
+            "subscription": subscription_version,
+            "creditcheck": creditcheck_version,
+            "checkout_session": checkout_session_version},
+        timeout=4)
